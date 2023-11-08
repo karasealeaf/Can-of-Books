4d2970fb45c3d7504b8462b8d34e2460c8e1f9aa
Samuel Draper <samueledraper@outlook.com>
2023-11-08 15:24:37 +0000

    add update movies

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <>
+      <h2>Books</h2>
+      <p>The ultimate book database</p>
+    </>
+  );
+}
---
+++ client/src/pages/Book.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Form from "../components/Form";
+
+export default function Book() {
+  const [book, setBook] = useState({});
+
+  const params = useParams();
+
+  useEffect(() => {
+    getBook();
+  }, []);
+
+  async function getBook() {
+    const API = `http://localhost:8080/books?_id=${params.id}`;
+    const res = await axios.get(API);
+    setBook(res.data[0]);
+  }
+
+  return (
+    <>
+      <div key={book._id}>
+        <h2>{book.title}</h2>
+        <img src={book.imgURL} />
+        {book.status ? <p>yes</p> : <p>no</p>}
+        {book.title && <Form book={book} setBook={setBook} />}
+      </div>
+    </>
+  );
+}
