b8da4a9a736bbd630976df25725ff0ba43213c1c
Andrea <andrea.vandyck88@gmail.com>
2023-11-08 15:54:59 +0000

    css adds

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,18 +4,20 @@ import Form from "../components/Form";
 export default function Home({ books, setBooks, deleteBook }) {
   return (
     <>
-      {books.map((book) => {
-        return (
-          <div key={book._id}>
-            <Link to={`/book/${book._id}`}>
-              <h2>{book.title}</h2>
-              <img src={book.imgURL} />
-            </Link>
-            {book.status ? <p>yes</p> : <p>no</p>}
-            <button onClick={() => deleteBook(book._id)}>Delete Book</button>
-          </div>
-        );
-      })}
+      <section id="carousel">
+        {books.map((book) => {
+          return (
+            <div className="booksection" key={book._id}>
+              <Link to={`/book/${book._id}`}>
+                <h2>{book.title}</h2>
+                <img src={book.imgURL} />
+              </Link>
+              {book.status ? <p>yes</p> : <p>no</p>}
+              <button onClick={() => deleteBook(book._id)}>Delete Book</button>
+            </div>
+          );
+        })}
+      </section>
       <Form books={books} setBooks={setBooks} />
     </>
   );
